3ba994a8a69e55fe15aa0f0d6439a7373a30e1e7
marunemo <chys3697@gmail.com>
2021-07-06 01:09:54 +0900

    link firebase data to list item
    - get restaurant data from firebase realtime database
    
    ! will fix infinity loop error !

--- restaurant/ListItem.js
+++ restaurant/ListItem.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import {Text, View, ScrollView, SafeAreaView, StyleSheet} from "react-native"
+import database from '@react-native-firebase/database';
 
 const itemActivity = () => {
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const [distance, setDistance] = useState("");
+    const [menu, setMenu] = useState([]);
+
+    database().ref("/식당/9월애").once("value").then(restData => {
+        data = restData.val();
+        setName(data["official_name"]);
+        setAddress(data["address"]);
+        setPhone(data["contact"]);
+        setDistance(data["distance"]);
+        setMenu(data["menu"]);
+    });
+
     return(
         <SafeAreaView style = {style.containter}>
             <ScrollView>
                 <View style = {style.partition}>
-                    <Text>Test</Text>
+                    <Text>이름 : {name}</Text>
+                    <Text>주소 : {address}</Text>
+                    <Text>번호 : {phone}</Text>
                 </View>
                 <View style = {style.partition}>
-                    <Text>Example</Text>
+                    <Text>거리 : {distance/1000}km</Text>
+                </View>
+                <View style = {style.partition}>
+                    <Text>{menu}</Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -26,7 +47,7 @@ const style = StyleSheet.create({
         borderWidth : 2,
         borderRadius : 25,
         margin : 5,
-        padding : 50,
+        padding : 10,
         alignItems : "center"
     }
 })
