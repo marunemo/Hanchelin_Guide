01a3f198ab3ac0496610add1b18255f83ffe575e
marunemo <chys3697@gmail.com>
2021-07-13 22:21:38 +0900

    Fixed autolinking dependencies error

---
+++ react-native.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    dependencies: {
+      'react-native-vector-icons': {
+        platforms: {
+          android: null, // disable Android platform, other platforms will still autolink
+          ios: null, // disable iOS platform, other platforms will still autolink
+        },
+      },
+      'react-native-webview': {
+        platforms: {
+          android: null, // disable Android platform, other platforms will still autolink
+          ios: null, // disable iOS platform, other platforms will still autolink
+        },
+      },
+    },
+  };
