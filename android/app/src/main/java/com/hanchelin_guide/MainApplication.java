8c5053c694a250646ab392705e72183731f1cb32
marunemo <chys3697@gmail.com>
2021-07-14 16:38:51 +0900

    merged with "[bug fixed] android webview error" commit
    - React Native CLI uses autolinking for native dependencies, but the following modules are linked manually
    - Invariant Violation: requireNativeComponent: "RNCWebView" was not found in the UIManager.

--- android/app/src/main/java/com/hanchelin_guide/MainApplication.java
+++ android/app/src/main/java/com/hanchelin_guide/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -27,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNCWebViewPackage());
           return packages;
         }
 
