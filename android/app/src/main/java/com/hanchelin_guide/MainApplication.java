1904aa9ed9be84521d78a8c46507bd262ed246ee
marunemo <chys3697@gmail.com>
2021-07-21 00:32:07 +0900

    [bug fixed] fixed android safe-area error

--- android/app/src/main/java/com/hanchelin_guide/MainApplication.java
+++ android/app/src/main/java/com/hanchelin_guide/MainApplication.java
@@ -13,8 +13,6 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -31,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new RNCWebViewPackage());
+          packages.add(new SafeAreaContextPackage());
           return packages;
         }
 
