1c8c2ec1a5eec1acf160b6c50cc8c8b27dbc7767
seokmin01 <22000666@handong.edu>
2021-08-09 17:01:23 +0900

    Update RestHome.js
    some change

--- restaurant/RestHome.js
+++ restaurant/RestHome.js
@@ -56,9 +56,13 @@ class Home extends Component {
         this.setState({ sortTerm: term })
         term == "가나다순" && this.setState({ data: this.state.data.sort((a, b) => a.name > b.name) })
         term == "추천순" && this.setState({ data: this.state.data.sort((a, b) => a.likes < b.likes) })
+        term == "리뷰개수순" && this.setState({ data: this.state.data.sort((a, b) => a.comments_count < b.comments_count) })
     }
     render() {
-        const filteredArr = this.state.data.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS)).filter(createFilter(this.state.switchValue ? '1' : '', 'delivery_availability')).filter(createFilter(this.state.category, 'category'))
+        const filteredArr = this.state.data
+        .filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+        .filter(createFilter(this.state.switchValue ? '1' : '', 'delivery_availability'))
+        .filter(createFilter(this.state.category, 'category'))
         return (
             <NativeBaseProvider>
                 <Select
@@ -74,6 +78,7 @@ class Home extends Component {
                 >
                     <Select.Item label="가나다순" value="가나다순" />
                     <Select.Item label="추천순" value="추천순" />
+                    <Select.Item label="리뷰개수순" value="리뷰개수순" />
                     <Select.Item label="별점순" value="별점순" />
                 </Select>
                 <HStack alignItems="center" space={1} m={1}>
