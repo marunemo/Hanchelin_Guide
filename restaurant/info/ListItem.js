51b1c4e3a6b89117d3448d0980327f99f1853e8f
seokmin01 <22000666@handong.edu>
2021-08-11 13:12:50 +0900

    스크린 이름 중복 오류 해결

--- restaurant/info/ListItem.js
+++ restaurant/info/ListItem.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from "react";
-import {Text, View, ScrollView, SafeAreaView, StyleSheet} from "react-native"
-import NaverMapView, {Marker} from "react-native-nmap";
-import {NativeBaseProvider} from "native-base";
+import React, { useState, useEffect } from "react";
+import { Text, View, ScrollView, SafeAreaView, StyleSheet } from "react-native"
+import NaverMapView, { Marker } from "react-native-nmap";
+import { NativeBaseProvider } from "native-base";
 import database from '@react-native-firebase/database';
-import {useNavigation} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import CommentButton from "./CommentModal";
 import MapScreen from "./MapScreen";
@@ -14,55 +14,55 @@ const Stack = createNativeStackNavigator();
 const MapView = (props) => {
     const navigation = useNavigation();
 
-    if(props.position["latitude"] == undefined)
-        return <View style = {style.mapView} />
-    
-    return(
+    if (props.position["latitude"] == undefined)
+        return <View style={style.mapView} />
+
+    return (
         <NaverMapView
-            style = {style.mapView}
-            center={{...props.position, zoom : 18}} 
-            compass = {false}
-            scaleBar = {true}
-            zoomControl = {true}
-            minZoomLevel = {6}
-            maxZoomLevel = {19}
-            onMapClick = {() => {navigation.navigate("식당 위치", {name : props.restName, coordinate : props.position})}}>
-                <Marker coordinate={props.position} />
+            style={style.mapView}
+            center={{ ...props.position, zoom: 18 }}
+            compass={false}
+            scaleBar={true}
+            zoomControl={true}
+            minZoomLevel={6}
+            maxZoomLevel={19}
+            onMapClick={() => { navigation.navigate("식당 위치", { name: props.restName, coordinate: props.position }) }}>
+            <Marker coordinate={props.position} />
         </NaverMapView>
     )
 }
 
 const RestComponent = (props) => {
     const restData = props.restData;
-    
+
     let menu = "";
-    if(restData["menu"] != undefined) {
-        for(var menuList of restData["menu"])
+    if (restData["menu"] != undefined) {
+        for (var menuList of restData["menu"])
             menu += menuList + "\n";
         menu = menu.substring(0, menu.length - 1)
     }
-    
+
     return (
         <>
-            <View style = {style.partition}>
+            <View style={style.partition}>
                 <Text style={style.contexts}><Text style={style.keyText}>이름 :</Text> {restData["official_name"]}</Text>
                 <Text style={style.contexts}><Text style={style.keyText}>주소 :</Text> {restData["address"]}</Text>
                 <Text style={style.contexts}><Text style={style.keyText}>번호 :</Text> {restData["contact"]}</Text>
             </View>
-            <View style = {style.partition}>
+            <View style={style.partition}>
                 <Text style={style.contexts}>
                     <Text style={style.keyText}>위치 정보</Text>
                 </Text>
                 <MapView
-                    restName = {restData["official_name"]}
-                    position={{latitude: restData["y"], longitude: restData["x"]}} />
+                    restName={restData["official_name"]}
+                    position={{ latitude: restData["y"], longitude: restData["x"] }} />
             </View>
-            <View style = {[style.partition, style.endMargin]}>
-                <Text style={[style.keyText, {lineHeight : 40, fontSize : 16}]}>ᐧ 메뉴</Text>
-                <Text style={({paddingLeft : 20})}>{menu}</Text>
+            <View style={[style.partition, style.endMargin]}>
+                <Text style={[style.keyText, { lineHeight: 40, fontSize: 16 }]}>ᐧ 메뉴</Text>
+                <Text style={({ paddingLeft: 20 })}>{menu}</Text>
             </View>
         </>
-    ); 
+    );
 }
 
 const RestaurantInfo = (props) => {
@@ -71,7 +71,7 @@ const RestaurantInfo = (props) => {
 
     useEffect(() => {
         database().ref(restDir).once("value").then(data => {
-            if(data) {
+            if (data) {
                 setData(data.val());
             }
         });
@@ -79,33 +79,33 @@ const RestaurantInfo = (props) => {
     }, []);
     console.log(restData);
 
-    return(
-        <SafeAreaView style = {style.containter}>
+    return (
+        <SafeAreaView style={style.containter}>
             <NativeBaseProvider>
                 <ScrollView>
-                    <RestComponent restData = {restData}/>
+                    <RestComponent restData={restData} />
                 </ScrollView>
-                <CommentButton restName = {restData["official_name"]}/>
+                <CommentButton restName={restData["official_name"]} />
             </NativeBaseProvider>
         </SafeAreaView>
     )
 }
 
-const ItemActivity = ({route}) => {
+const ItemActivity = ({ route }) => {
     const RestInfo = () => {
-        return <RestaurantInfo restId = {route.params.restId} />
+        return <RestaurantInfo restId={route.params.restId} />
     }
 
     return (
         <Stack.Navigator>
             <Stack.Screen
-                name = "식당 정보"
-                component = {RestInfo}
-                options = {{headerShown : false}} />
+                name="식당 정보 화면"
+                component={RestInfo}
+                options={{ headerShown: false }} />
             <Stack.Screen
-                name = "식당 위치"
-                component = {MapScreen}
-                options = {{headerShown : false}} />
+                name="식당 위치"
+                component={MapScreen}
+                options={{ headerShown: false }} />
         </Stack.Navigator>
     )
 }
@@ -113,27 +113,27 @@ const ItemActivity = ({route}) => {
 export default ItemActivity;
 
 const style = StyleSheet.create({
-    containter : {
-        height : "100%"
+    containter: {
+        height: "100%"
     },
-    contexts : {
-        lineHeight : 20
+    contexts: {
+        lineHeight: 20
     },
-    keyText : {
-        fontWeight : "bold",
+    keyText: {
+        fontWeight: "bold",
     },
-    partition : {
-        borderWidth : 2,
-        borderRadius : 25,
-        margin : 5,
-        paddingVertical : 20,
-        paddingHorizontal : 30
+    partition: {
+        borderWidth: 2,
+        borderRadius: 25,
+        margin: 5,
+        paddingVertical: 20,
+        paddingHorizontal: 30
     },
-    mapView : {
-        width : "100%",
-        aspectRatio : 1
+    mapView: {
+        width: "100%",
+        aspectRatio: 1
     },
-    endMargin : {
-        marginBottom : 100
+    endMargin: {
+        marginBottom: 100
     }
 })
