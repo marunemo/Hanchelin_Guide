098dff9210b147998b38c1544241a6577b8b4f64
marunemo <chys3697@gmail.com>
2021-11-04 17:30:55 +0900

    feat: add some effect on login screen
    - activate react-native-reanimated
      - apply sample animation to logo image
    - make background more dark

--- jin/screens/Authentication.js
+++ jin/screens/Authentication.js
@@ -1,11 +1,24 @@
 // 로그인 페이지
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Image, ImageBackground } from 'react-native';
 import Text from '../../defaultSetting/FontText';
 import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming } from 'react-native-reanimated';
 
 export default function Authentication(props) {
+  const rotation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotateZ: `${rotation.value}deg` }],
+    };
+  });
+
+  useEffect(() => {
+    rotation.value = withRepeat(withTiming(10), 6, true)
+  }, [])
+
   return (
     <View style={styles.screen}>
       <ImageBackground
@@ -13,7 +26,9 @@ export default function Authentication(props) {
         source={require('../../images/custom_background.png')}
         resizeMode="cover"
       >
+        <Animated.View style={animatedStyle}>
         <Image style={styles.img} source={require('../../images/appicon.png')} />
+        </Animated.View>
         <Text style={styles.title} bold>한슐랭 가이드</Text>
         <Text style={styles.subtitle}>한동인들을 위한 맛집 어플</Text>
         <View style={styles.centerMargin} />
@@ -54,6 +69,6 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   centerMargin: {
-    height: 180
+    height: 100
   }
 });
