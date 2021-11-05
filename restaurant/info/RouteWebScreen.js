5c596c9b0ac7b6037216eafa2893698766cadd1f
marunemo <chys3697@gmail.com>
2021-11-05 14:36:38 +0900

    fix: refactoring

--- restaurant/info/RouteWebScreen.js
+++ restaurant/info/RouteWebScreen.js
@@ -2,12 +2,11 @@ import React from 'react';
 import WebView from 'react-native-webview';
 
 const RouteWebScreen = ({ route }) => {
-  console.log(route.params)
-  const {name, coordinate, myPosition} = route.params;
+  const { name, coordinate, myPosition } = route.params;
   const restPath = `https://m.map.naver.com/route.nhn?menu=route&sname=내 위치&sx=${myPosition.longitude}&sy=${myPosition.latitude}&ename=${name}&ex=${coordinate.longitude}&ey=${coordinate.latitude}&pathType=1&showMap=true`;
-  console.log(restPath)
   return (
     <WebView
+      onShouldStartLoadWithRequest={() => true}
       source={{ uri: restPath }}
     />
   )
