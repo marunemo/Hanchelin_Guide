54c6b7219c5c5d7d52db2ec19bf4cf6f8ec82ac0
marunemo <chys3697@gmail.com>
2021-11-05 14:24:20 +0900

    fix: latitude and longitude swap bug

--- restaurant/info/RouteWebScreen.js
+++ restaurant/info/RouteWebScreen.js
@@ -4,7 +4,7 @@ import WebView from 'react-native-webview';
 const RouteWebScreen = ({ route }) => {
   console.log(route.params)
   const {name, coordinate, myPosition} = route.params;
-  const restPath = `https://m.map.naver.com/route.nhn?menu=route&sname=내 위치&sx=${myPosition.latitude}&sy=${myPosition.longitude}&ename=${name}&ex=${coordinate.latitude}&ey=${coordinate.longitude}&pathType=0&showMap=true`;
+  const restPath = `https://m.map.naver.com/route.nhn?menu=route&sname=내 위치&sx=${myPosition.longitude}&sy=${myPosition.latitude}&ename=${name}&ex=${coordinate.longitude}&ey=${coordinate.latitude}&pathType=0&showMap=true`;
   console.log(restPath)
   return (
     <WebView
