3307b01af7816aa07120232683b9b27d237ed14d
seokmin01 <79754613+seokmin01@users.noreply.github.com>
2021-11-10 23:22:35 +0900

    로그인 화면 배경 변경 및 ios 앱 이름 변경

--- jin/screens/Authentication.js
+++ jin/screens/Authentication.js
@@ -2,29 +2,34 @@
 
 import React from 'react';
 import { StyleSheet, View, Image, ImageBackground } from 'react-native';
+import { Center, NativeBaseProvider } from "native-base";
 import Text from '../../defaultSetting/FontText';
 import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
 
 export default function Authentication(props) {
   return (
-    <View style={styles.screen}>
-      <ImageBackground
-        style={styles.background}
-        source={require('../../images/custom_background.png')}
-        resizeMode="cover"
-      >
-        <Image style={styles.img} source={require('../../images/appicon.png')} />
-        <Text style={styles.title} bold>한슐랭 가이드</Text>
-        <Text style={styles.subtitle}>한동인들을 위한 맛집 어플</Text>
-        <View style={styles.centerMargin} />
-        <GoogleSigninButton
-          style={{ width: 230, height: 48 }}
-          size={GoogleSigninButton.Size.Wide}
-          onPress={props.onGoogleButtonPress}
-          disabled={props.disabled}
-        />
-      </ImageBackground>
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.screen}>
+        <ImageBackground
+          style={styles.background}
+          source={require('../../images/bg.jpg')}
+          resizeMode="cover"
+        >
+          <Image style={styles.img} source={require('../../images/appicon.png')} />
+          <Center style={styles.titleBox}>
+            <Text style={styles.title} bold>한슐랭 가이드</Text>
+            <Text style={styles.subtitle}>한동인들을 위한 맛집 어플</Text>
+          </Center>
+          <View style={styles.centerMargin} />
+          <GoogleSigninButton
+            style={{ width: 230, height: 48 }}
+            size={GoogleSigninButton.Size.Wide}
+            onPress={props.onGoogleButtonPress}
+            disabled={props.disabled}
+          />
+        </ImageBackground>
+      </View>
+    </NativeBaseProvider>
   );
 }
 
@@ -32,15 +37,20 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
+  titleBox: {
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.55)',
+    paddingVertical: 14,
+  },
   title: {
     color: '#ffffff',
-    fontSize: 35,
-    marginBottom: 10,
+    fontSize: 44,
+    marginBottom: 12,
+    fontFamily: 'ELANDChoiceB',
   },
   subtitle: {
     color: '#ffffff',
-    fontSize: 18,
-    marginBottom: 30,
+    fontSize: 21,
   },
   img: {
     marginTop: 20,
@@ -54,6 +64,6 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   centerMargin: {
-    height: 100
+    height: "35%"
   }
 });
